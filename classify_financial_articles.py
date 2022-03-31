e7493a79cd838d2518753399254142088a3ff260
jpromanonet <programming@jpromano.net>
2022-03-30 23:02:25 -0300

    new model

---
+++ classify_financial_articles.py
@@ -0,0 +1,9 @@
+# Let's import some libraries
+from gravityai import gravityai as grav
+import pickle
+import pandas as pd
+
+# Let's create some pickle files
+model = pickle.load(open(''))
+tfidf_vectorizer = pickle.load(open(''))
+label_encoder = pickle.load(open(''))
